d786b389210910878f3aa4ddcece00a1f89bde18
Adam Rohde <adamrohde@outlook.com>
2020-11-25 14:49:57 -0600

    configured dark and light theme colors

--- res/js/theme.js
+++ res/js/theme.js
@@ -1,27 +1,41 @@
 let root = document.querySelector(":root");
 
 document.getElementById("dark-mode").onclick = function () {
-    root.style.setProperty("--theme-color-nav", "rgb(73, 73, 73)");
-    root.style.setProperty("--theme-color-back", "#131313");
-    root.style.setProperty("--theme-color-keys", "#0E4F9E");
-    root.style.setProperty("--theme-color-keys-secondary", "#8FB3E0");
+    root.style.setProperty("--theme-color-nav", "rgb(19, 13, 13)");
+    root.style.setProperty("--theme-color-back", "rgb(59, 52, 52)");
+    root.style.setProperty("--theme-color-scoreBars", "rgb(255, 0, 0)");
     root.style.setProperty("--theme-color-text", "rgb(255, 255, 255)");
     root.style.setProperty("--theme-color-burger", "rgb(255, 255, 255)");
+    root.style.setProperty("--theme-color-hands", "tan");
+    root.style.setProperty("--theme-color-buttons", "rgb(230, 230, 233)");
 };
 
 document.getElementById("light-mode").onclick = function () {
     root.style.setProperty("--theme-color-nav", "rgb(213, 213, 213)");
     root.style.setProperty("--theme-color-back", "rgb(255, 255, 255)");
-    root.style.setProperty("--theme-color-keys", "rgb(220, 220, 220)");
-    root.style.setProperty("--theme-color-keys-secondary", "rgb(191, 191, 191)");
+    root.style.setProperty("--theme-color-scoreBars", "rgb(255, 0, 0)");
     root.style.setProperty("--theme-color-text", "rgb(0, 0, 0)");
     root.style.setProperty("--theme-color-burger", "rgb(64, 64, 64)");
+    root.style.setProperty("--theme-color-hands", "tan");
+    root.style.setProperty("--theme-color-buttons", "rgb(64, 64, 64)");
 };
 
-// --theme-color-nav: #494949;
-//     --theme-color-back: #131313;
-//     --theme-color-keys: #0e4f9e;
-//     --theme-color-keys-secondary: #8fb3e0;
+// Dark Theme Colors
+//     --theme-color-nav: rgb(19, 13, 13);
+//     --theme-color-back: rgb(59, 52, 52);
+//     --theme-color-scoreBars: rgb(255, 0, 0);
+//     --theme-color-keys-secondary: rgb(191, 191, 191);
+//     --theme-color-text: rgb(255, 255, 255);
+//     --theme-color-burger: rgb(255, 255, 255);
+//     --theme-color-hands: tan;
+//     --theme-color-buttons: rgb(230, 230, 233);
 
-//     --theme-color-text: #fafcff;
-//     --theme-color-burger: #f1f1f1;
+// Light Theme Colors
+//      --theme-color-nav: rgb(213, 213, 213);
+//      --theme-color-back: rgb(245, 245, 245);
+//      --theme-color-scoreBars: rgb(255, 0, 0);
+//      --theme-color-keys-secondary: rgb(191, 191, 191);
+//      --theme-color-text: rgb(0, 0, 0);
+//      --theme-color-burger: rgb(64, 64, 64);
+//      --theme-color-hands: tan;
+//      --theme-color-buttons: lightgray;
