a94af58a4e2f8787f864802675a888e9320ee50d
Adam Rohde <adamrohde@outlook.com>
2020-11-25 20:43:32 -0600

    user preferences

--- res/js/index.js
+++ res/js/index.js
@@ -1,65 +1,64 @@
-//console.log("Hello there");
-/*
-Order of operations index
-
-0 - load page
-1 - Start game by clicking screen
-2a - count to 3
-2b - the user selects rock paper or scissors
-3 - UI displays the user and the computers pick after time has elapsed
-4 - determines winner
-5 - adds score the left or right side depending on winner
-6 - displays the winnter
-7 - a click resets and starts game over again
-*/
+// Order of operations index
+// 0 - load page, also handles modal and drop down events
+// 1 - Start game by clicking screen
+// 2a - count to 3
+// 2b - the user selects rock paper or scissors
+// 3 - UI displays the user and the computers pick after time has elapsed
+// 4 - determines winner
+// 5 - adds score the left or right side depending on winner
+// 6 - displays the winnter
+// 7 - a click resets and starts game over again
 
 const resultMap = [
     ["t", "c", "u"],
     ["u", "t", "c"],
     ["c", "u", "t"],
 ];
-
 const result = {
     t: "Tie",
     u: "You win",
     c: "You lose",
 };
-
 const playOptions = [
     "<i class='fas fa-hand-paper'></i>",
     "<i class='fas fa-hand-rock'></i>",
     "<i class='fas fa-hand-scissors'></i>",
 ];
-
 let playIndex;
 let gameResultsOutcome;
 let timeleft;
 const gameStatusAndCountDown = document.getElementById("game-countdown-and-status");
 
-//0
-window.addEventListener("load", () => {
-    document.getElementById("the-humans-play").innerHTML = " ";
-    document.getElementById("the-computers-play").innerHTML = " ";
+// -------------------------------------- Modal --------------------------------------
+$("#staticBackdrop").on("shown.bs.modal", () => {
+    document.body.removeEventListener("click", countDown);
 });
 
-$("#myDropdown").on("hide.bs.dropdown", () => {
-    console.log("show dropdown");
+$("#staticBackdrop").on("hidden.bs.modal", () => {
     document.body.addEventListener("click", countDown);
 });
 
-//1
-document.body.addEventListener("click", countDown);
+// -------------------------------------- Dropdown --------------------------------------
+$("#myDropdown").on("hidden.bs.dropdown", () => {
+    console.log("show dropdown");
+    document.body.addEventListener("click", countDown);
+});
 
 document.getElementById("dropdownMenuButton").onclick = () => {
     document.body.removeEventListener("click", countDown);
 };
 
-$(".dropdownMenuButton").on("hide.bs.dropdown", function () {
-    console.log("Drop down has been hidden");
-    document.body.addEventListener("click");
+// -------------------------------------- 0 --------------------------------------
+window.addEventListener("load", () => {
+    document.getElementById("the-humans-play").innerHTML = " ";
+    document.getElementById("the-computers-play").innerHTML = " ";
+
+    gameStatusAndCountDown.textContent = "Click!";
 });
 
-//2a
+// -------------------------------------- 1 --------------------------------------
+document.body.addEventListener("click", countDown);
+// -------------------------------------- 2a --------------------------------------
 function countDown() {
     document.getElementById("the-humans-play").innerHTML = " ";
     document.getElementById("the-computers-play").innerHTML = " ";
@@ -69,28 +68,28 @@ function countDown() {
     document.body.removeEventListener("click", countDown);
 
     var downloadTimer = setInterval(function () {
-        //2b Rock Play
+        // -------------------------------------- 2b Rock --------------------------------------
         document.getElementById("shoot-rock").addEventListener("click", () => {
             if (timeleft <= 4) {
                 randomNum = Math.floor(Math.random() * 3);
                 playIndex = 1;
             }
         });
-        //2b Paper Play
+        // -------------------------------------- 2b Paper --------------------------------------
         document.getElementById("shoot-paper").addEventListener("click", () => {
             if (timeleft <= 4) {
                 randomNum = Math.floor(Math.random() * 3);
                 playIndex = 0;
             }
         });
-        //2b Scissors Play
+        // -------------------------------------- 2b Scissors --------------------------------------
         document.getElementById("shoot-scissors").addEventListener("click", () => {
             if (timeleft <= 4) {
                 randomNum = Math.floor(Math.random() * 3);
                 playIndex = 2;
             }
         });
-        //3
+        // -------------------------------------- 3 --------------------------------------
         if (timeleft >= 3) {
             if (randomNum == null) {
                 gameStatusAndCountDown.textContent = "to slow";
@@ -102,22 +101,22 @@ function countDown() {
                 document.getElementById("the-humans-play").innerHTML = myPlay;
                 document.getElementById("the-computers-play").innerHTML = computerPlay;
                 gameResultsOutcome = resultMap[randomNum][playIndex];
-                //4
+                // -------------------------------------- 4 --------------------------------------
                 if (gameResultsOutcome == "u") {
-                    //5
+                    // -------------------------------------- 5 --------------------------------------
                     let grid_item = document.createElement("grid-item");
                     document.getElementById("add_point_for_human").appendChild(grid_item);
                 } else if (gameResultsOutcome == "c") {
-                    //5
+                    // -------------------------------------- 5 --------------------------------------
                     let grid_item = document.createElement("grid-item");
                     document.getElementById("add_point_for_computer").appendChild(grid_item);
                 }
-                //6
+                // -------------------------------------- 6 --------------------------------------
                 gameStatusAndCountDown.textContent = result[gameResultsOutcome];
                 document.body.addEventListener("click", countDown);
                 clearInterval(downloadTimer);
             }
-            //2a
+            // -------------------------------------- 2a --------------------------------------
         } else {
             timeleft += 1;
             gameStatusAndCountDown.textContent = timeleft;
--- res/js/theme.js
+++ res/js/theme.js
@@ -1,24 +1,60 @@
 let root = document.querySelector(":root");
 
-document.getElementById("dark-mode").onclick = function () {
+document.getElementById("dark-mode").onclick = () => {
     root.style.setProperty("--theme-color-nav", "rgb(19, 13, 13)");
     root.style.setProperty("--theme-color-back", "rgb(59, 52, 52)");
     root.style.setProperty("--theme-color-scoreBars", "rgb(255, 0, 0)");
     root.style.setProperty("--theme-color-text", "rgb(255, 255, 255)");
     root.style.setProperty("--theme-color-burger", "rgb(255, 255, 255)");
-    root.style.setProperty("--theme-color-hands", "tan");
     root.style.setProperty("--theme-color-buttons", "rgb(230, 230, 233)");
 };
 
-document.getElementById("light-mode").onclick = function () {
+document.getElementById("light-mode").onclick = () => {
     root.style.setProperty("--theme-color-nav", "rgb(213, 213, 213)");
     root.style.setProperty("--theme-color-back", "rgb(255, 255, 255)");
     root.style.setProperty("--theme-color-scoreBars", "rgb(255, 0, 0)");
     root.style.setProperty("--theme-color-text", "rgb(0, 0, 0)");
     root.style.setProperty("--theme-color-burger", "rgb(64, 64, 64)");
-    root.style.setProperty("--theme-color-hands", "tan");
     root.style.setProperty("--theme-color-buttons", "lightgray");
 };
+
+document.getElementById("select-tone-6").onclick = () => {
+    console.log("skin tone 6");
+    root.style.setProperty("--theme-color-hands", "#604437");
+};
+document.getElementById("select-tone-5").onclick = () => {
+    console.log("skin tone 5");
+    root.style.setProperty("--theme-color-hands", "#a3602f");
+};
+document.getElementById("select-tone-4").onclick = () => {
+    console.log("skin tone 4");
+    root.style.setProperty("--theme-color-hands", "#ca9062");
+};
+document.getElementById("select-tone-3").onclick = () => {
+    console.log("skin tone 3");
+    root.style.setProperty("--theme-color-hands", "#e2b084");
+};
+document.getElementById("select-tone-2").onclick = () => {
+    console.log("skin tone 2");
+    root.style.setProperty("--theme-color-hands", "#ffddb9");
+};
+document.getElementById("select-tone-1").onclick = () => {
+    console.log("skin tone 1");
+    root.style.setProperty("--theme-color-hands", "#ffce02");
+};
+
+const customThemeBackgroundColor = document.querySelector(".custom-theme-bg-color[type=color]");
+//const lightThemeBackgroundColor = document.querySelector(".light-theme-bg-color[type=color]");
+
+customThemeBackgroundColor.addEventListener("change", (e) => {
+    console.log("Custom " + e.target.value);
+    root.style.setProperty("--theme-color-back", e.target.value);
+    //document.documentElement.style.setProperty(colorVariable, e.target.value);
+});
+
+function saveLocalStorage() {
+    console.log("save local storage");
+}
 
 // Dark Theme Colors
 //     --theme-color-nav: rgb(19, 13, 13);
@@ -39,3 +75,10 @@ document.getElementById("light-mode").onclick = function () {
 //      --theme-color-burger: rgb(64, 64, 64);
 //      --theme-color-hands: tan;
 //      --theme-color-buttons: lightgray;
+
+// --skin-tone-6: #604437;
+// --skin-tone-5: #a3602f;
+// --skin-tone-4: #ca9062;
+// --skin-tone-3: #e2b084;
+// --skin-tone-2: #ffddb9;
+// --skin-tone-1: #ffce02;
